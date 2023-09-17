da262f1ec6760959e877c0cfa30f6555895cd7b6
Benjamin Addai-Boateng <b.addaiboateng@korba365.com>
2023-09-17 14:46:09 +0000

    update

--- lib/first_package.dart
+++ lib/first_package.dart
@@ -7,6 +7,8 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 
+  int addMore(int value, int add) => value + add;
+
   int minOne(int value) => value - 1;
 
   final sc = StringsCheck();
